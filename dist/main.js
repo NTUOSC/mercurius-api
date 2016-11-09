13036e810ccd6a2898c4b91ff33a57daf653d802
Squirrel <kanade123321@gmail.com>
2016-11-09 15:19:52 +0800

    simple page

---
+++ dist/main.js
@@ -0,0 +1,34 @@
+$(function () {
+    socket.on('card attach', function(data) {
+        $('#name').text(data);
+        $('#accept').prop( "disabled", false);
+        $('#reject').prop( "disabled", false);
+    });
+
+    socket.on('message', function(data) {
+        $('#message').text(data);
+    });
+
+    // Connection ON
+    socket.on('connect', function () {
+        $('#message').text('Connection ON');
+    });
+    socket.on('reconnect', function () {
+        $('#message').text('Connection ON');
+    });
+
+    // Connection OFF
+    socket.on('disconnect', function () {
+        $('#message').text('Connection DOWN');
+    });
+
+    $('#accept').click(function () {
+        socket.emit('accept');
+        $('#accept').prop( "disabled", true);
+    });
+
+    $('#reject').click(function () {
+        socket.emit('reject');
+        $('#reject').prop( "disabled", true);
+    });
+});
