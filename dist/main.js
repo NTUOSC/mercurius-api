57ed77830151b2cb548cf9e9148ffe1d0b5213b3
Katrina Chan <katrina.hyc@gmail.com>
2016-12-06 16:43:44 +0800

    Commit before merge

--- dist/main.js
+++ dist/main.js
@@ -9,38 +9,79 @@ var l = function (string) {
     return string.toLocaleString("zh_tw");
 };
 
+var clear_message = function () {
+  $$('#message-box section').forEach(function (it) { it.innerHTML = '' });
+}
+
+var change_button_state = function (s) {
+  var valid_action = { 'activate' : true, 'deactivate' : false };
+  if (valid_action[s]) {
+    $('#accept').setAttribute('disabled', true);
+    $('#reject').setAttribute('disabled', true);
+    $('#dismiss').removeAttribute('disabled');
+  } else {
+    $('#accept').removeAttribute('disabled');
+    $('#reject').removeAttribute('disabled');
+    $('#dismiss').setAttribute('disabled', true);
+  }
+}
+
+var timeout;
+
 var socket = io('http://localhost:8080');
 socket.on('card attach', function(data) {
   $('#name').innerHTML = data;
-  $('#accept').removeAttribute('disabled');
-  $('#reject').removeAttribute('disabled');
+  // $('#name').innerHTML = data.id;
+  // $('#message').innerHTML = data.type;
+  change_button_state('activate');
 });
 
 socket.on('message', function(data) {
-  $('#message').innerHTML = l(data);
+  $('#name').innerHTML = '';
+  $('#message').innerHTML = data;
+  change_button_state('deactivate');
+  timeout = setTimeout(clear_message, 5000);
 });
 
 // Connection ON
 socket.on('connect', function () {
-  $('#message').innerHTML = l('online');
+  $('#status').innerHTML = l('online');
+  change_button_state('deactivate');
 });
 socket.on('reconnect', function () {
-  $('#message').innerHTML = l('online');
+  $('#status').innerHTML = l('online');
+  change_button_state('deactivate');
 });
 
 // Connection OFF
 socket.on('disconnect', function () {
-  $('#message').innerHTML = l('offline');
+  $('#status').innerHTML = l('offline');
+  change_button_state('deactivate');
+  clear_message();
 });
 
 $('#accept').addEventListener('click', function (ev) {
   socket.emit('accept');
-  $('#accept').setAttribute('disabled', true);
-  $('#reject').setAttribute('disabled', true);
+  change_button_state('deactivate');
 });
 
 $('#reject').addEventListener('click', function (ev) {
   socket.emit('reject');
-  $('#accept').setAttribute('disabled', true);
-  $('#reject').setAttribute('disabled', true);
+  change_button_state('deactivate');
+});
+
+$('#dismiss').addEventListener('click', function (ev) {
+  change_button_state('deactivate');
+  clear_message();
+  clearTimeout(timeout);
+});
+
+$$('button').forEach(function (it) {
+  // active style
+  it.addEventListener('touchstart', function (ev) { ev.preventDefault(); ev.target.className = 'active'; });
+  it.addEventListener('mousedown', function (ev) { ev.preventDefault(); ev.target.className = 'active'; });
+
+  // normal style
+  it.addEventListener('touchend', function (ev) { ev.preventDefault(); ev.target.className = ''; });
+  it.addEventListener('mouseup', function (ev) { ev.preventDefault(); ev.target.className = ''; });
 });
